07fbb3400e7293b2aba45a429bec676a8e95919c
michael.ressler <w46m127@csci112.cs.montana.edu>
2019-05-03 01:50:00 -0600

    initial commit

---
+++ lab0.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+//Michael Ressler
+//lab0
+//1/11/2019
+//
+//first program, just to learn how to print
+int main(void) {
+	//print hello world
+	printf("hello world\n");
+
+	return(0);
+}
